7e3bdc78f113c8beacfe8e1f570d3062800b68fd
HP <qaasim.yahya17@gmail.com>
2024-05-20 15:59:07 +0530

    updated a comment

--- src/main.java
+++ src/main.java
@@ -2,5 +2,8 @@ public class main {
     public static void main(String[] args) {
 
          Power_Ranger obj1 = new Power_Ranger("")
+
+                 // comment
+                // hi batch 86
     }
 }
